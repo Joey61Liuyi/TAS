9d5940b1dd1b255644c5e6c1c0ceaa5eea6d6136
JoeyLau <csyiliu@comp.polyu.edu.hk>
2021-08-06 20:49:52 +0800

    modified the cell based search as one block

--- lib/models/addtional_models.py
+++ lib/models/addtional_models.py
@@ -750,6 +750,88 @@ def ShuffleNetG3():
     return ShuffleNet(cfg)
 
 
+class Block(nn.Module):
+    '''expand + depthwise + pointwise + squeeze-excitation'''
+
+    def __init__(self, in_planes, out_planes, expansion, stride):
+        super(Block, self).__init__()
+        self.stride = stride
+
+        planes = expansion * in_planes
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, groups=planes, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(
+            planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes)
+
+        self.shortcut = nn.Sequential()
+        if stride == 1 and in_planes != out_planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=1,
+                          stride=1, padding=0, bias=False),
+                nn.BatchNorm2d(out_planes),
+            )
+
+        # SE layers
+        self.fc1 = nn.Conv2d(out_planes, out_planes//16, kernel_size=1)
+        self.fc2 = nn.Conv2d(out_planes//16, out_planes, kernel_size=1)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        shortcut = self.shortcut(x) if self.stride == 1 else out
+        # Squeeze-Excitation
+        w = F.avg_pool2d(out, out.size(2))
+        w = F.relu(self.fc1(w))
+        w = self.fc2(w).sigmoid()
+        out = out * w + shortcut
+        return out
+
+
+class EfficientNet(nn.Module):
+    def __init__(self, cfg, num_classes=10):
+        super(EfficientNet, self).__init__()
+        self.cfg = cfg
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.layers = self._make_layers(in_planes=32)
+        self.linear = nn.Linear(cfg[-1][1], num_classes)
+
+    def _make_layers(self, in_planes):
+        layers = []
+        for expansion, out_planes, num_blocks, stride in self.cfg:
+            strides = [stride] + [1]*(num_blocks-1)
+            for stride in strides:
+                layers.append(Block(in_planes, out_planes, expansion, stride))
+                in_planes = out_planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layers(out)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def EfficientNetB0():
+    # (expansion, out_planes, num_blocks, stride)
+    cfg = [(1,  16, 1, 2),
+           (6,  24, 2, 1),
+           (6,  40, 2, 2),
+           (6,  80, 3, 2),
+           (6, 112, 3, 1),
+           (6, 192, 4, 2),
+           (6, 320, 1, 2)]
+    return EfficientNet(cfg)
+
+
 resnet_book = {
     '8': resnet8_cifar,
     '14': resnet14_cifar,
@@ -819,6 +901,8 @@ def is_resnet(name):
         return 'squeezenet'
     elif name.startswith('shufflenet'):
         return 'shufflenet'
+    elif name.startswith('efficientnetb0'):
+        return 'efficientnetb0'
 
 
 def create_cnn_model(name, dataset="cifar100", total_epochs = 160, model_path = None, use_cuda = False):
@@ -898,6 +982,17 @@ def create_cnn_model(name, dataset="cifar100", total_epochs = 160, model_path = 
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
                                                         [total_epochs / 2, total_epochs * 3 / 4, total_epochs],
                                                         gamma=0.1, last_epoch=-1)
+
+    elif is_resnet(name) == 'efficientnetb0':
+        efficientnetb0_model = EfficientNetB0()
+        model = efficientnetb0_model
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9, weight_decay=5e-4)
+        # Assuming optimizer uses lr = 0.05 for all groups
+        # lr = 0.05     if epoch < 30
+        # lr = 0.005    if 30 <= epoch < 60
+        # lr = 0.0005   if 60 <= epoch < 90
+        # ...
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=200, gamma=0.1)
 
     # copy to cuda if activated
     if use_cuda:
